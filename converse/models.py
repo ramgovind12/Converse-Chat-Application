063351670db8f849920a5a88a8ea84144db37767
Ram Govind V <ramgovindvi.in@gmail.com>
2023-12-24 10:00:28 +0530

    views and templates

--- converse/models.py
+++ converse/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length = 250)
+    
+class Message(models.Model):
+    author = models.ForeignKey(Author,on_delete = models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    
+    
--- converse/views.py
+++ converse/views.py
@@ -1,8 +1,27 @@
+from datetime import datetime
+import random
 from django.shortcuts import render, redirect
 from django.http import HttpRequest,HttpResponse
+import json
+from typing import AsyncGenerator
 
 
 # Create your views here.
 
+def lobby(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        if username:
+            request.session['username'] == username
+        else:
+            names = [
+                "Penguin", "Sunflower", "Butterfly", "Rainbow", "Dragon", "Star", "Moon", "Cupcake", "Sparrow", "Whale"
+            ]
+            request.session['username'] = f"{random.choice(names)}-{hash(datetime.now().timestamp())}"
+        return redirect('chat')
+    return render(request,'lobby.html')
+
 def chat(request: HttpRequest) -> HttpResponse:
-    
+    if not request.session.get('username'):
+        return redirect('lobby')
+    return render(request,'chat.html')
